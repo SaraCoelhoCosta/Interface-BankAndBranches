b19e8dad5a34707dff784aa48ab1cd5dfa148eb1
Lara <larafabian061@gmail.com>
2023-05-03 23:18:23 -0300

    Atualização das tabelas

--- js/table.js
+++ js/table.js
@@ -10,9 +10,34 @@ function listCity() {
         idCell.textContent = item.id;
         const nomeCell = document.createElement("td");
         nomeCell.textContent = item.nome;
+        const actionsCell = document.createElement("td"); // Célula para os botões
+        const editButton = document.createElement("button"); // Botão de Editar
+        
+        editButton.textContent = "Editar";
+        editButton.addEventListener("click", () => {
+            // Adicione aqui a lógica para editar o item selecionado
+            console.log(`Editar ${item.id} - ${item.nome}`);
+        });
+
+        const space = document.createElement("span");
+        space.textContent = " ";
+        
+        const deleteButton = document.createElement("button"); // Botão de Excluir
+        deleteButton.textContent = "Excluir";
+        deleteButton.addEventListener("click", () => {
+            // Adicione aqui a lógica para excluir o item selecionado
+            console.log(`Excluir ${item.id} - ${item.nome}`);
+        });
+        
+        actionsCell.appendChild(editButton);
+        actionsCell.appendChild(space);
+        // actionsCell.appendChild(document.createElement("br")); // Adiciona uma quebra de linha
+        actionsCell.appendChild(deleteButton);
         row.appendChild(idCell);
-            row.appendChild(nomeCell);
-            tableBody.appendChild(row);
+        row.appendChild(nomeCell);
+        row.appendChild(actionsCell); // Adiciona a célula dos botões na linha
+        tableBody.appendChild(row);
+           
         });
     })
     .catch(error => console.error(error));
