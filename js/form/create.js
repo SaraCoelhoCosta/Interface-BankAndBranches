bf29cb8644d46a91f69386f4138f33ba720e7b95
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2023-05-12 09:58:21 -0300

    Largura dos formulários

---
+++ js/form/create.js
@@ -0,0 +1,77 @@
+function createCity() {
+    fetch(`http://127.0.0.1:8000/cidades`)
+        .then(response => response.json())
+        .then(data => {
+            // Selecionar o elemento select
+            const select = document.querySelector("#cidades");
+
+            // Limpar o elemento select
+            select.innerHTML = "";
+
+            // Adicionar uma opção vazia como a primeira opção
+            const emptyOption = document.createElement("option");
+            emptyOption.value = "";
+            emptyOption.textContent = "Selecione uma cidade";
+            select.appendChild(emptyOption);
+
+            // Iterar sobre os dados e criar uma opção para cada nome
+            data.forEach(item => {
+                const option = document.createElement("option");
+                option.value = item.id;
+                option.textContent = item.nome;
+                select.appendChild(option);
+            });
+        });
+}
+
+function createBranch() {
+    fetch(`http://127.0.0.1:8000/agencias`)
+        .then(response => response.json())
+        .then(data => {
+            // Selecionar o elemento select
+            const select = document.querySelector("#agencias");
+
+            // Limpar o elemento select
+            select.innerHTML = "";
+
+            // Adicionar uma opção vazia como a primeira opção
+            const emptyOption = document.createElement("option");
+            emptyOption.value = "";
+            emptyOption.textContent = "Selecione uma agência";
+            select.appendChild(emptyOption);
+
+            // Iterar sobre os dados e criar uma opção para cada nome
+            data.forEach(item => {
+                const option = document.createElement("option");
+                option.value = item.id;
+                option.textContent = item.descricao;
+                select.appendChild(option);
+            });
+        });
+}
+
+function createAccount() {
+    fetch(`http://127.0.0.1:8000/contas`)
+        .then(response => response.json())
+        .then(data => {
+            // Selecionar o elemento select
+            const select = document.querySelector("#tipo_contas");
+
+            // Limpar o elemento select
+            select.innerHTML = "";
+
+            // Adicionar uma opção vazia como a primeira opção
+            const emptyOption = document.createElement("option");
+            emptyOption.value = "";
+            emptyOption.textContent = "Selecione um tipo de conta";
+            select.appendChild(emptyOption);
+
+            // Iterar sobre os dados e criar uma opção para cada nome
+            data.forEach(item => {
+                const option = document.createElement("option");
+                option.value = item.id;
+                option.textContent = item.tipo_conta;
+                select.appendChild(option);
+            });
+        });
+}
